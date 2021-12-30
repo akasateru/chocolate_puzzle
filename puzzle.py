1280d756bf7836f7f03ef19353640ac39b78ea24
ohatanaoki <akasateru@gmail.com>
2021-12-30 22:01:43 +0900

    puzzle一旦完

--- puzzle.py
+++ puzzle.py
@@ -19,9 +19,7 @@ puzzle = [[[1,1,1],[1,0,1]],
           [[1,1,1,1],[0,1,0,0]],
           [[0,0,1],[0,0,1],[1,1,1]]]
 
-def culclate(result,result_pzl,number,i,flag):
-  if flag==False:
-    return 0
+def culclate(result,result_pzl,number,i,flag,run_once):
   i+=1
   print("\n深さ:",i)
   if number==[] and min(list(map(lambda x: min(x), result)))==1:
@@ -29,14 +27,9 @@ def culclate(result,result_pzl,number,i,flag):
     print(pd.DataFrame(result_pzl))
     print("-----------------------")
     return 0
-  elif number==[]:
-    print("-------fault----------")
-    return 0
   else:
     for puz_i in number:
       if flag==True:
-        print("number_hajime:",number)
-        print("puz_i:",puz_i)
         # hakoを順に動かす
         for row_f in range(6-len(puzzle[puz_i])+1):
           for col_f in range(10-len(puzzle[puz_i][0])+1):
@@ -54,22 +47,24 @@ def culclate(result,result_pzl,number,i,flag):
               result = list(map(lambda x,y: list(map(lambda a,b: a+b, x,y)), result, hako))
               result_pzl = list(map(lambda x,y: list(map(lambda a,b: a+b, x,y)), result_pzl, hako_pzl))
               number.remove(puz_i)
-              print("result_pzl:",pd.DataFrame(result_pzl))
+              print("result_pzl:\n",pd.DataFrame(result_pzl))
               print("number:",number)
-              culclate(result,result_pzl,number,i,flag)
-              flag=False
+              culclate(result,result_pzl,number,i,flag,run_once)
               break
             if row_f+row==5 and col_f+col==9 and max(list(map(lambda x: max(x), result_kari))) > 1:
-              flag=False
+              print("\n--------fault---------")
+              run_once=1
               return 0
           else:
             continue
           break
         flag=False
       else:
-        print("-----------fault")
         return 0
 
 number1 = [0,1,2,3,4,5,6,7,8,9,10,11]
 number2 = [1,2,3,4,5,6,7,8,9,10,11,0]
-culclate(result,result_pzl,number2,i=0,flag=True)
+number3 = [2,3,4,5,6,7,8,9,10,11,0,1]
+
+
+culclate(result,result_pzl,number1,i=0,flag=True,run_once=0)
